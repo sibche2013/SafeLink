fc742488b88ae41ebd64f027f80ec81042e50867
hamedp71 <170614544+hamedp-71@users.noreply.github.com>
2025-08-27 01:03:20 +0330

    Update cl.py

--- cl.py
+++ cl.py
@@ -27,11 +27,11 @@ with open(CONF_PATH,"r") as file_client_set:
         f=json.load(file_client_set)
         test_link_=f["core"]["test_url"]
 TEXT_PATH="normal.txt"
-LINK_PATH=[] # [ "link1" , "link2" , ... ]
+LINK_PATH=[] # [ "https://raw.githubusercontent.com/darkvpnapp/CloudflarePlus/refs/heads/main/proxy" , "link2" , ... ]
 FIN_PATH="final.txt"
 FIN_CONF=[]
-CHECK_LOC=False
-CHECK_IRAN=False
+CHECK_LOC=True
+CHECK_IRAN=True
 CHECK_HOST_IRANIAN_NODES = [
     "ir1.node.check-host.net",  # Tehran, AS44244 Mobile Communication Company of Iran (MCI)
     "ir2.node.check-host.net",  # Tehran, AS12880 Telecommunication Infrastructure Company (TIC زیرساخت)
@@ -1928,5 +1928,6 @@ with open(FIN_PATH,"w") as f:
     except Exception as e:
         print(f"Unexpected error writing to {FIN_PATH}: {e}")
 exit()
+
 
 
